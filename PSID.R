cd3f158d7453b52c43f7664aaf1c0e88159f8b59
gayan18 <47056915+gayan18@users.noreply.github.com>
2019-01-27 19:41:13 +0530

    Update PSID.R

--- PSID.R
+++ PSID.R
@@ -1,4 +1,3 @@
-#Gayan
 psidData1 = read.csv("PSID.csv", header = TRUE)
 psidData = psidData1[!is.na(psidData1$educatn),]
 summary(psidData)
@@ -15,7 +14,6 @@ points(kc$centers[,2:3],col=1:2, pch=8,cex=2)
 plot(psidData[,4:8], col=kc$cluster)
 points(kc$centers[,3:4],col=1:2, pch=8,cex=2)
 
-#Yasura
 psid = read.csv("PSID.csv",header = TRUE)
 
 psid
@@ -43,7 +41,7 @@ hist(psid$educatn)
 
 hist(psid$age)
 
-#Madhavi
+#####
 psidData = read.csv("PSID.csv", header = TRUE)
 psid =  psidData[rowSums(is.na(psidData)) ==0,]
 psid
